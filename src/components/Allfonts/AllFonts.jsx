1abd4fcc105b0638bf4d364800130ba42b19ff96
Mika Otter <mi.otter.side@gmail.com>
2024-06-17 20:50:11 +0200

    Done : Test fonts

--- src/components/Allfonts/AllFonts.jsx
+++ src/components/Allfonts/AllFonts.jsx
@@ -3,13 +3,59 @@ import s from "./AllFonts.module.scss";
 import { useSelector, useDispatch } from "react-redux";
 import Card from "../Card/Card";
 import useHandleFonts from "../../hooks/useHandleFonts";
+import axios from "../../app/api/axios";
+import cn from "classnames";
 
 export default function AllFonts() {
   const dispatch = useDispatch();
   const fonts = useSelector((state) => state.fonts.value);
   const choosedFonts = useSelector((state) => state.choosedFonts.value);
+  const userId = useSelector((state) => state.auth.userId);
+  const [fontRatings, setFontRatings] = useState([]);
+  const [sortedFonts, setSortedFonts] = useState("");
 
   const handleFonts = useHandleFonts(fonts, choosedFonts, dispatch);
+
+  const getRate = async (userId) => {
+    try {
+      const res = await axios.post("/fonts/rate/getAll", { userId });
+      const fontRates = res.data.data;
+      setFontRatings(fontRates);
+    } catch (err) {
+      console.error("FAILED : Try to get all rates => ", err);
+    }
+  };
+
+  const mergeRatingsWithFonts = (fonts, ratings) => {
+    const ratingsMap = ratings.reduce((map, rate) => {
+      map[rate.fontName] = rate.rating;
+      return map;
+    }, {});
+
+    return fonts.map((font) => ({
+      ...font,
+      rating: ratingsMap[font.name] || 0,
+    }));
+  };
+
+  const sortFonts = (sortedFonts) => {
+    const mergedFonts = mergeRatingsWithFonts(fonts, fontRatings);
+    console.log(mergedFonts, "mergedFonts");
+    if (sortedFonts === "rate") {
+      return mergedFonts.sort((a, b) => b.rating - a.rating);
+    } else if (sortedFonts === "name") {
+      return mergedFonts.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortedFonts === "id") {
+      return mergedFonts.sort((a, b) => b.id - a.id);
+    }
+    return mergedFonts;
+  };
+
+  useEffect(() => {
+    if (userId) {
+      getRate(userId);
+    }
+  }, [userId]);
 
   return (
     <>
@@ -17,15 +63,45 @@ export default function AllFonts() {
         <div className={s.container__title}>
           <div className={s.title}>
             <h3>All your fonts</h3>
-            <div className={s.title__paramas}>
+            <div className={s.sort}>
               <span>Sort by</span>
-              <span className={s.title__paramas__selector}>Rate</span>
+
+              <button
+                type="button"
+                className={cn(
+                  s.sort__selector,
+                  sortedFonts === "name" && s.active
+                )}
+                onClick={() => setSortedFonts("name")}
+              >
+                Name
+              </button>
+              <button
+                type="button"
+                className={cn(
+                  s.sort__selector,
+                  sortedFonts === "rate" && s.active
+                )}
+                onClick={() => setSortedFonts("rate")}
+              >
+                Rate
+              </button>
+              <button
+                type="button"
+                className={cn(
+                  s.sort__selector,
+                  sortedFonts === "id" && s.active
+                )}
+                onClick={() => setSortedFonts("id")}
+              >
+                Most Recent
+              </button>
             </div>
           </div>
         </div>
         <div className={s.content}>
           <div className={s.content__ctn}>
-            {fonts.map((font, i) => (
+            {sortFonts(sortedFonts).map((font, i) => (
               <Card
                 font={font}
                 i={i}
--- src/components/testText/testText.jsx
+++ src/components/testText/testText.jsx
@@ -37,25 +37,18 @@ export default function TestText() {
 
   return (
     <>
-      <div className={s.layout}></div>
       <section>
         <div className={s.test}>
           <div className={s.test__title}>
             <h2>Test fonts</h2>
           </div>
           <div className={s.test__text}>
-            <div className={s.border__left}>
-              <Border />
-            </div>
             <input
               type="text"
               name="test"
               value={test}
               onChange={(e) => setTest(e.target.value)}
             />
-            <div className={s.border__right}>
-              <Border />
-            </div>
           </div>
           <div className={s.test__buttons}>
             <input
@@ -86,50 +79,48 @@ export default function TestText() {
             <label htmlFor="smallLine">Small line</label>
           </div>
         </div>
-      </section>
-      <section className={s.test__section}>
-        {choosedFonts.length === 0 ? (
-          <>
-            <p className={s.nochoosedfont}>
-              Please select a font in your folder...
-            </p>
-            <button type="button">Folder</button>
-          </>
-        ) : (
-          choosedFonts.map((font, index) => (
-            <div className={s.test__view} key={`font-${index}`}>
-              {checkboxes.bigCheckbox && (
-                <span
-                  key={`big-${font.name}`}
-                  className={cn(`font-${font.name}`, s.test__view__big)}
-                >
-                  {test}
+        <div className={s.test__section}>
+          {choosedFonts.length === 0 ? (
+            <>
+              <p className={s.nochoosedfont}>
+                Please select a font in your folder...
+              </p>
+              <button type="button">Folder</button>
+            </>
+          ) : (
+            choosedFonts.map((font, index) => (
+              <div className={s.test__view} key={`font-${index}`}>
+                {checkboxes.bigCheckbox && (
+                  <span
+                    key={`big-${font.name}`}
+                    className={cn(`font-${font.name}`, s.test__view__big)}
+                  >
+                    {test}
+                  </span>
+                )}
+                {checkboxes.mediumCheckbox && (
+                  <span
+                    key={`medium-${font.name}`}
+                    className={cn(`font-${font.name}`, s.test__view__medium)}
+                  >
+                    {test}
+                  </span>
+                )}
+                {checkboxes.smallCheckbox && (
+                  <span
+                    key={`small-${font.name}`}
+                    className={cn(`font-${font.name}`, s.test__view__small)}
+                  >
+                    {test}
+                  </span>
+                )}
+                <span key={index} className={s.test__view__typeface}>
+                  {font.name}
                 </span>
-              )}
-              {checkboxes.mediumCheckbox && (
-                <span
-                  key={`medium-${font.name}`}
-                  className={cn(`font-${font.name}`, s.test__view__medium)}
-                >
-                  {test}
-                </span>
-              )}
-              {checkboxes.smallCheckbox && (
-                <span
-                  key={`small-${font.name}`}
-                  className={cn(`font-${font.name}`, s.test__view__small)}
-                >
-                  {test}
-                </span>
-              )}
-              <span key={index} className={s.test__view__typeface}>
-                {font.name}
-              </span>
-            </div>
-          ))
-        )}
-
-        <Folder />
+              </div>
+            ))
+          )}
+        </div>
       </section>
     </>
   );
